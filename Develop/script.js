c3403d90e7980ef1c2739e2b7f2881d18c9accf7
KeadenM <keadenmeacham@gmail.com>
2023-03-12 17:18:47 -0600

    added prompts and constant for all characters

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,14 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
+  var pwlength = 0;
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#$%&'()*+,-./:;<=>?@]\[^_`{|}~";  
   prompt("How many characters? (Needs to be at least 8 characters)")
+  prompt("Include Uppercase letters?")
+  prompt("Include Lowercase letters?")
+  prompt("Include numbers?")
+  prompt("Include special characters")
+  
 }
 // Write password to the #password input
 function writePassword() {
